61e936bf3f15acf9b2070becb35849b6599ab03e
Nana Chuto <mail@nanachuto.com>
2023-04-13 10:57:36 +0200

    Set state variable

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 import contacts from "./contacts.json";
+import { useState } from "react";
 
 function App() {
+  const [ironContacts, setContacts] = useState([
+    contacts[0],
+    contacts[1],
+    contacts[2],
+    contacts[2],
+    contacts[3],
+  ]);
+
+  console.log(contacts);
+
   return <div className="App"></div>;
 }
 export default App;
